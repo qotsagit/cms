2d7a02ed16dff99c0e1f66bdab46488da63e205e
Rafał Żygadło <qotsa@op.pl>
2018-02-20 16:10:48 +0100

    Delete dist.sh

--- dist.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir sql
-cd sql
-mysqldump --databases qotsa2_cms -uqotsa2 -pfhkhz8536 >cms.sql
-
-cd ..
-tar -zcvf update.cms.tar.gz admin ctrls cron libs models sql views alter.php system.config.php
-#tar -zcvf install.tar.gz * .htaccess
